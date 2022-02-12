b1bc9f1fae51a1399b70eb9eab75520c30400f01
heyp999 <99363173+heyp999@users.noreply.github.com>
2022-02-11 22:34:52 -0800

    Update test.py

--- test.py
+++ test.py
@@ -6,12 +6,12 @@ DRIVER_PATH ="/usr/local/bin/chromedriver"
 
 if __name__ == "__main__":
     # 设置浏览器
-    options &#61; Options()
+    options = Options()
     options.add_argument("--no-sandbox")
     options.add_argument("--headless")  # 无头参数
     options.add_argument("--disable-gpu")
     # 启动浏览器
-    driver &#61; Chrome(executable_path&#61;DRIVER_PATH, options&#61;options)
+    driver = Chrome(executable_path=DRIVER_PATH, options=options)
     # 访问目标URL
     driver.get("https://www.baidu.com/")
     print(driver.page_source)
