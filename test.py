a93901a77d5e56fd3cb4a3a909f5e5bf2ba4b852
heyp999 <99363173+heyp999@users.noreply.github.com>
2022-02-11 22:32:22 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,19 @@
-#!/usr/bin/env bash
-import time
-from selenium import webdriver
-import os  
-from selenium.webdriver.common.keys import Keys  
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
 
-# "$ xvfb-run python test.py", this is how you run this script
-chrome_options = webdriver.ChromeOptions()
-# below trick saved my life
-chrome_options.add_argument('--no-sandbox')
+DRIVER_PATH ="/usr/local/bin/chromedriver"
 
-# set the folder where you want to save your file
-prefs = {'download.default_directory' : os.getcwd()}
-chrome_options.add_experimental_option('prefs', prefs)
 
-# Optional argument, if not specified will search path.
-driver = webdriver.Chrome('/usr/local/bin/chromedriver',chrome_options=chrome_options)  
-chrome_options=chrome_options
-
-# Scraping steps
-driver.get("http://pypi.python.org/pypi/selenium")
-time.sleep(3)
-driver.find_element_by_css_selector("#content > div.section > table > tbody > tr.odd > td:nth-child(1) > span > a:nth-child(1)").click()
-time.sleep(3)
-print('Finished!')
-driver.quit()
+if __name__ == "__main__":
+    # 设置浏览器
+    options &#61; Options()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--headless")  # 无头参数
+    options.add_argument("--disable-gpu")
+    # 启动浏览器
+    driver &#61; Chrome(executable_path&#61;DRIVER_PATH, options&#61;options)
+    # 访问目标URL
+    driver.get("https://www.baidu.com/")
+    print(driver.page_source)
+    driver.close()
+    driver.quit()
