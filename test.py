05c293131829f7a0b90f53de8724827ece1715b7
heyp999 <99363173+heyp999@users.noreply.github.com>
2022-02-11 22:52:32 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,16 @@
-from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.options import Options
-
-DRIVER_PATH ="/usr/local/bin/chromedriver"
-
-
-if __name__ == "__main__":
-    # 设置浏览器
-    options = Options()
-    options.add_argument("--no-sandbox")
-    options.add_argument("--headless")  # 无头参数
-    options.add_argument("--disable-gpu")
-    # 启动浏览器
-    driver = Chrome(executable_path=DRIVER_PATH, options=options)
-    # 访问目标URL
-    driver.get("https://www.baidu.com/")
-    print(driver.page_source)
-    driver.close()
-    driver.quit()
+from selenium import webdriver
+option = webdriver.ChromeOptions()
+# 无头模式
+option.add_argument('headless')
+# 沙盒模式运行
+option.add_argument('no-sandbox')
+# 大量渲染时候写入/tmp而非/dev/shm
+option.add_argument('disable-dev-shm-usage')
+# 指定驱动路径
+browser = webdriver.Chrome( options=option)
+# 访问百度
+browser.get('http://www.baidu.com/')
+# 打印标题
+print(browser.title)
+# 关闭浏览器
+browser.quit()
